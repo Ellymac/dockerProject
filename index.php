1be3b6005908897014972b63a137bf5246a62702
Ellymac <cschnell67@gmail.com>
2019-02-20 00:31:08 +0100

    oui

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
         <div>
           <?php
             echo 'bonjouuu';
-            $servername = "127.0.0.1";
+            $servername = "127.0.0.1:3306";
             $username = "root";
             $password = "root";
 
